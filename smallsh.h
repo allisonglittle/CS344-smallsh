951f09291d4118ef9d95142c891c4b9d5316baf9
Alli <allisonglittle@gmail.com>
2022-02-05 21:13:14 -0800

    Updated parse user input function to parse input and output files

--- smallsh.h
+++ smallsh.h
@@ -8,7 +8,9 @@
 /* Maximum number of arguments */
 #define MAXARGS 512
 
+/* --------------------------------------------------------------------------------------------------------- */
 /* Struct to hold user's command */
+/* --------------------------------------------------------------------------------------------------------- */
 struct userInput {
 	char* command;
 	char* userArgs[MAXARGS];
@@ -17,7 +19,9 @@ struct userInput {
 	bool runBackground;
 };
 
+/* --------------------------------------------------------------------------------------------------------- */
 /* Takes in user input command line and parses into the user input struct */
+/* --------------------------------------------------------------------------------------------------------- */
 struct userInput* parseCommand(char* line) {
 
 	// Allocate memory for user command
@@ -30,40 +34,81 @@ struct userInput* parseCommand(char* line) {
 	char* saveptr;
 
 	// First token: command
-	char* token = strtok_r(line, " ", &saveptr);
+	char* token = strtok_r(line, " \n", &saveptr);
 	cmd->command = calloc(strlen(token) + 1, sizeof(char)); // allocate memory
 	strcpy(cmd->command, token); // copy token into the user input command
 
 	// Second set of tokens: arguments
+	token = strtok_r(NULL, " \n", &saveptr);
 	// Move through the arguments array
 	for (int i = 0; i < MAXARGS; i++) {
-		token = strtok_r(token, " ", &saveptr);
-		// If the token is null or input/output symbols, break out of loop
-		if (strcmp(token, "<") != 0) {
+		// If the token is null, input/output or background symbols, break out of loop
+		if (token == NULL) {
+			// There are no more tokens in the command, return the user input 
+			return cmd;
+		}
+		else if (strcmp(token, "<") == 0) {
 			// There is an input file
 			break;
 		}
-		else if (strcmp(token, ">") != 0) {
+		else if (strcmp(token, ">") == 0) {
 			// There is an output file
 			break;
 		}
-		else if (token == NULL) {
-			break;
+		else if (strcmp(token, "&") == 0) {
+			// The command should run in the background
+			cmd->runBackground = true;
+			// Return the user input
+			return cmd;
 		}
 		// Assign the token string to the arguments array
 		cmd->userArgs[i] = calloc(strlen(token) + 1, sizeof(char));
 		strcpy(cmd->userArgs[i], token);
+		// Next token
+		token = strtok_r(NULL, " \n", &saveptr);
 	}
 
 	// If the token is an input token, save the input file
+	if (strcmp(token, "<") == 0) {
+		// Read in the input file
+		token = strtok_r(NULL, " \n", &saveptr);
+		cmd->inputFile = calloc(strlen(token) + 1, sizeof(char)); // allocate memory
+		strcpy(cmd->inputFile, token); // copy token into the user input command
+		// Read next token
+		token = strtok_r(NULL, " \n", &saveptr);
+		if (token == NULL) {
+			// No more tokens, return command
+			return cmd;
+		}
+	}
 	
-	token = strtok_r(NULL, " >", &saveptr);
+	// If the token is an output token, save the output file
+	if (strcmp(token, ">") == 0) {
+		// Read in the output file
+		token = strtok_r(NULL, " \n", &saveptr);
+		cmd->outputFile = calloc(strlen(token) + 1, sizeof(char)); // allocate memory
+		strcpy(cmd->outputFile, token); // copy token into the user output command
+		// Read next token
+		token = strtok_r(NULL, " \n", &saveptr);
+		if (token == NULL) {
+			// No more tokens, return command
+			return cmd;
+		}
+	}
+
+	// If the token is the background symbol, set the command to run in the background
+	if (strcmp(token, "&") == 0) {
+		// The command should run in the background
+		cmd->runBackground = true;
+	}
 
 	// Return the user command with parsed data
 	return cmd;
 }
 
+/* --------------------------------------------------------------------------------------------------------- */
 /* Prompt user for a command input, parse command and execute */
+/* --------------------------------------------------------------------------------------------------------- */
 void getUserInput() {
 	// Prompt user
 	printf(": ");
