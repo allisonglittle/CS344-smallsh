4845455ed69e3eb3cfb3830b44c2e3ae016d40d0
Alli <allisonglittle@gmail.com>
2022-02-07 20:19:36 -0800

    Signal handler for ignoring SIGINT

--- smallsh.c
+++ smallsh.c
@@ -6,6 +6,11 @@ Assignment 3 - smallsh
 #include "smallsh.h"
 
 int main() {
+	// Set up signal handlers
+	struct sigaction ignore_action = { 0 }, SIGTSTP_action = { 0 };
+	// Parent process should ignore SIGINT
+	ignore_action.sa_handler = SIG_IGN;
+	sigaction(SIGINT, &ignore_action, NULL);
 
 	while (1) {
 		getUserInput();
